726db20b7fb33e7faa16026b2c427014f61c3e92
Tom Berghuis <thomas.berghuis@gmail.com>
2017-02-25 10:40:12 +1100

    .

--- client/components/PollResults.jsx
+++ client/components/PollResults.jsx
@@ -23,15 +23,6 @@ class PollResults extends Component {
     }
 
 
-    //lifecycle method after render
-    
-    // if poll null return
-
-    // else kill the element, redraw
-
-    // look up steven grider rallycoding vid + link in notes.txt
-
-
     render() {
 
 
@@ -39,7 +30,7 @@ class PollResults extends Component {
             <div class="row">
                 <div class="col-12">
                     d3 shit goes in here
-                    {this.poll && "fdsgdsgsdgdsgds"}
+                    {this.poll && <PollResultChart></PollResultChart>}
                 </div>
             </div>
         );
@@ -47,3 +38,52 @@ class PollResults extends Component {
 };
 
 export default PollResults;
+
+
+class PollResultChart extends Component {
+
+
+
+    componentDidMount() {
+        // call d3
+    }
+
+    shouldComponentUpdate(nextProps) {
+        // call d3
+
+        // return false
+    }
+
+    render() {
+        return (
+            <svg>
+                <text y="20" fill="black">Hello, World!</text>
+            </svg>
+        );
+    }
+}
+
+
+
+class PollOption {
+
+
+   // maxVotes
+   // totalVotes
+
+
+
+    constructor(option, votes){
+        this.option = option;
+        this.votes = votes;
+    }
+
+    // computed percentage
+
+
+    // sort
+   static sort(a,b){
+        return b.votes-a.votes || (a.option > b.option ? 1 : -1);
+    }
+
+}
